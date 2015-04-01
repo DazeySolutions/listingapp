43c1b90d00bf8e0a2c3a50d6ce72db5680e3eb84
Mark Dazey <markdazey@gmail.com>
2015-04-01 10:10:03 -0400

    Update UserProperties.java
    update to use singleton

--- src/main/java/com/ds/listing/properties/UserProperties.java
+++ src/main/java/com/ds/listing/properties/UserProperties.java
@@ -2,36 +2,29 @@ package com.ds.listing.properties;
 
 import java.io.IOException;
 import java.util.Properties;
+import javax.inject.Singleton;
+import javax.annotation.PostConstruct;
 
 /**
  * Created by bithack on 3/31/15.
  */
+ @Singleton
 public class UserProperties {
-    private static UserProperties _instance = null;
-
     private Properties props;
-
     private boolean valid = true;
 
-    protected UserProperties(){
+    @PostConstruct
+    private void init(){
         try {
             props = new Properties();
             props.load(getClass().getResourceAsStream("/META-INF/kmhenry.properties"));
         } catch (IOException e) {
-            //squash error
             valid = false;
         }
     }
 
     public boolean isValid(){
         return valid;
-    }
-
-    public static UserProperties getInstance(){
-        if(_instance == null){
-            _instance = new UserProperties();
-        }
-        return _instance;
     }
 
     public String getValue(String key){
