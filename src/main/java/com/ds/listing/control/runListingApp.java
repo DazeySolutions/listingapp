ca8f04a5447d7d9daedf5cf2d4d248036b0c3968
Mark Dazey <markdazey@gmail.com>
2015-04-21 18:02:41 +0000

    update

--- src/main/java/com/ds/listing/control/runListingApp.java
+++ src/main/java/com/ds/listing/control/runListingApp.java
@@ -22,6 +22,27 @@ public class runListingApp {
     @Inject
     private eBayAuth auth;
     
+    @PersistenceContext(unitName="primary")
+    private EntityManager em;
+
+    @Schedule(second = "*/15", minute = "*", hour = "*")
+    public void checkForNewListings() {
+        System.out.println("Test");
+        eBayListingService listingService = new eBayListingService(auth.getApiContext());
+        listingService.getCurrentListings();
+        //List<Listing> listings = findListings();
+        //for(Listing item : listings){
+//            if(listingService.addListing(item)){
+//                item.setStatus(true);
+//                em.persist(item);
+//            }
+//        }
+     }
+    private List<Listing> findListings(){
+        Query query = em.createQuery("Select l from Listing l where l.status = false");
+        return (List<Listing>) query.getResultList();
+    }
+    
     @PostConstruct
     private void startup() {
         System.out.println("Server Started");
--- src/main/java/com/ds/listing/control/scheduled/Tasks.java
+++ src/main/java/com/ds/listing/control/scheduled/Tasks.java
@@ -1,47 +1,47 @@
-package com.ds.listing.control.scheduled;
+// package com.ds.listing.control.scheduled;
 
-import com.ds.listing.model.Listing;
-import com.ds.listing.services.eBayListingService;
-import com.ds.listing.properties.eBayAuth;
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
-import javax.inject.Inject;
-import javax.ejb.Startup;
+// import com.ds.listing.model.Listing;
+// import com.ds.listing.services.eBayListingService;
+// import com.ds.listing.properties.eBayAuth;
+// import javax.ejb.Schedule;
+// import javax.ejb.Singleton;
+// import javax.persistence.EntityManager;
+// import javax.persistence.PersistenceContext;
+// import javax.persistence.Query;
+// import java.util.List;
+// import javax.inject.Inject;
+// import javax.ejb.Startup;
 
-/**
- * Scheduled Tasks
- * Created by bithack on 3/31/15.
- */
-@Singleton
-@Startup
-public class Tasks {
+// /**
+//  * Scheduled Tasks
+//  * Created by bithack on 3/31/15.
+//  */
+// @Singleton
+// @Startup
+// public class Tasks {
 
-    @Inject
-    private eBayAuth auth;
+//     // @Inject
+//     // private eBayAuth auth;
 
-    @PersistenceContext(unitName="primary")
-    private EntityManager em;
+//     // @PersistenceContext(unitName="primary")
+//     // private EntityManager em;
 
-    @Schedule(second = "*/15", minute = "*", hour = "*")
-    public void checkForNewListings() {
-        System.out.println("Test");
-        eBayListingService listingService = new eBayListingService(auth.getApiContext());
-        listingService.getCurrentListings();
-        //List<Listing> listings = findListings();
-        //for(Listing item : listings){
-//            if(listingService.addListing(item)){
-//                item.setStatus(true);
-//                em.persist(item);
-//            }
-//        }
-     }
-     private List<Listing> findListings(){
-        Query query = em.createQuery("Select l from Listing l where l.status = false");
-        return (List<Listing>) query.getResultList();
-    }
+//     // @Schedule(second = "*/15", minute = "*", hour = "*")
+//     // public void checkForNewListings() {
+//     //     System.out.println("Test");
+//     //     eBayListingService listingService = new eBayListingService(auth.getApiContext());
+//     //     listingService.getCurrentListings();
+//         //List<Listing> listings = findListings();
+//         //for(Listing item : listings){
+// //            if(listingService.addListing(item)){
+// //                item.setStatus(true);
+// //                em.persist(item);
+// //            }
+// //        }
+//      }
+//     //  private List<Listing> findListings(){
+//     //     Query query = em.createQuery("Select l from Listing l where l.status = false");
+//     //     return (List<Listing>) query.getResultList();
+//     // }
 
-}
+// }
