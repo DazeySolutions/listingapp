e94a3353a60d17d99b66e70022aa2d4a2258da6b
Mark Dazey <markdazey@gmail.com>
2015-04-22 20:04:51 +0000

    changes to ui

---
+++ gruntsrc/js/custom.js
@@ -0,0 +1,110 @@
+var appDependencies = ['ui.router', 'toaster', 'ngAnimate', 'ngLodash', 'restangular'];
+var ngListApp = angular.module("ngListApp", appDependencies);
+
+angular.isUndefinedOrNull = function undefinedOrNull(value){
+    return angular.isUndefined(value) || value === null;
+};
+angular.isUndefinedOrNullOrEmpty = function undefinedOrNull(value){
+    return angular.isUndefined(value) || value === null || value === "";
+};
+ngListApp.filter('to_trusted', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]);
+ngListApp.config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl('/rest');
+});
+ngListApp.run('$rootScope', '$state', 'Auth', function($rootScope, $state, Auth){
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+        if(!Auth.isLoggedIn()){
+            event.preventDefault();
+            $state.go('login');
+        }
+    });
+})
+.factory('Auth', function(){
+    var user;
+    return {
+        setUser : function(aUser){
+            user = aUser;
+        },
+        isLoggedIn : function(){
+            return (user)?user:false;
+        }
+    }
+})
+.controller('LoginController', ['$scope', 'Auth', 'restangular','$state', function($scope, Auth, Restangular, $state){
+    $scope.login = function login(){
+        Restangular.post({user:$scope.userName, password:$scope.password}).then(function(data){
+           Auth.setUser({'loggedIn':true});
+           $state.go('default');
+        });
+    };
+}]);
+
+/**
+* UI ROUTER CONFIG FILE
+* router.js
+*/
+ngListApp.config(['$stateProvider','$urlRouterProvider','$rootScope', function($stateProvider, $urlRouterProvider, $rootScope){
+   
+    $urlRouterProvider.when('',['$state', function($state){
+        $state.go('site',{page:'home',controller:'HomePageController'});
+    }]);
+    $urlRouterProvider.when('/',['$state', function($state){
+        $state.go('site',{page:'home',controller:'HomePageController'});
+    }]);
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('default', {
+            url:'/',
+            templateUrl: 'partials/home.html',
+            controller: 'HomePageController'
+        })
+        .state('login',{
+            url:'/login',
+            templateUrl: 'partials/login.html',
+            controller: 'LoginController'
+        })
+        .state('list', {
+            url:'/list',
+            templateUrl: 'partials/list.html',
+            controller: 'ListMainController'
+        })
+        .state('list.new',{
+            url:'/list/new',
+            templateUrl: 'partials/list/new.html',
+            controller: 'NewListController'
+        })
+        .state('list.unsold',{
+            url:'/list/unsold',
+            templateUrl: 'partials/list/unsold.html',
+            controller: 'UnsoldListController'
+        })
+        .state('list.saved',{
+            url:'/list/saved',
+            templateUrl: 'partials/list/saved.html',
+            controller: 'SavedListController'
+        });
+}] );
+ngListApp.config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true);
+}]);
+
+/**
+* CONTROLLERS FILE
+* controllers.js
+*/
+ngListApp.controller('SiteController', ['$scope', 'toaster', '$window', '$http', '$stateParams', '$state', function ($scope, toaster, $window, $http, $stateParams, $state){
+    $scope.init = function init(){
+
+    };
+    $scope.init();
+}]);
+ngListApp.controller('HomePageController', ['$scope', '$http', '$stateParams', '$window','lodash', '$timeout', function($scope, $http, $stateParams, $window, lodash, $timeout){
+    $scope.init =  function init(){
+       
+    };
+    $scope.init();
+}]);
