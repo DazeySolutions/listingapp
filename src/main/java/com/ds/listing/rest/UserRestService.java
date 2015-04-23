95d86a12a5767cea5bab309c0881495917273f09
Mark Dazey <markdazey@gmail.com>
2015-04-23 14:11:41 +0000

    fix query

--- src/main/java/com/ds/listing/rest/UserRestService.java
+++ src/main/java/com/ds/listing/rest/UserRestService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.FormParam;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
