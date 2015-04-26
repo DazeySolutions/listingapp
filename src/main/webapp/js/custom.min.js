f88b0a1deeb135d738fa8bd48584414726a07f6d
Mark Dazey <markdazey@gmail.com>
2015-04-26 16:27:34 +0000

    update

--- src/main/webapp/js/custom.min.js
+++ src/main/webapp/js/custom.min.js
@@ -461,7 +461,7 @@ ngListApp.controller('UnsoldListController', ['$scope', '$http', '$stateParams',
                 as.ItemDimensions.Weight = as.ItemDimensions.Weight.toString();
             }
             item.book.weightMajor = parseInt(as.ItemDimensions.Weight);
-            item.book.weightMinor = Math.ceil((parseFloat(as.ItemDimensions.Weight)*16) % 16);
+            item.book.weightMinor = Math.ceil((parseFloat(as.ItemDimensions.Weight)*16) % 16)+.01;
             if(angular.isUndefinedOrNullOrEmpty(as.ItemDimensions.Height) || as.ItemDimensions.Height == 0){
                 item.book.depth = "8.5"
             }else{
